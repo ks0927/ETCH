81aff8d208c2ac58154d0e72c781e49397fe26f8
DESKTOP-DLOPF19\SSAFY <rladbstn0425@gmail.com>
2025-07-30 09:40:13 +0900

    init

---
+++ src/main/java/com/ssafy/etch/EtchApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.etch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/etch/EtchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.etch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
