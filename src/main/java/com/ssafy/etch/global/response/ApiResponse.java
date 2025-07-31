911285e92af4308dbff7a39fefefe0987b768710
DESKTOP-DLOPF19\SSAFY <rladbstn0425@gmail.com>
2025-07-31 13:48:10 +0900

    feat: ApiResponse 구현

---
+++ src/main/java/com/ssafy/etch/global/response/ApiResponse.java
@@ -0,0 +1,27 @@
+package com.ssafy.etch.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private T data;
+    private String message;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, null);
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return new ApiResponse<>(true, data, message);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, null, message);
+    }
+}
+
