adc301437bc3f4e700853454b6c7b29e3b7fdba9
hyunji1132 <hyunji1132@gmail.com>
2025-08-03 00:39:39 +0900

    feat: add scheduled job for top10 calculation

--- etch/backend/batch-server/batch_scheduler.py
+++ etch/backend/batch-server/batch_scheduler.py
@@ -8,6 +8,8 @@ from dotenv import load_dotenv, find_dotenv
 # 배치 스케쥴 목록
 # 1. news api 등록
 from fetch_news import fetch_and_save
+# 2. top10 기업 추출 및 redis 저장
+from cache_for_redis import fetch_and_store_top10
 
 # Logging
 logging.basicConfig(
@@ -29,11 +31,22 @@ def job_news():
     except Exception as e:
         logger.exception("Batch가 실패했습니다: %s", e)
 
+# Top 10 기업에 대한 스케쥴링
+def job_top10():
+    try:
+        fetch_and_store_top10()
+        logger.info("Top 10 기업에 대한 Batch가 끝났습니다.")
+    except Exception as e:
+        logger.exception("Top 10 기업에 대한 Batch가 실패했습니다: %s", e)
+
 if __name__ == "__main__":
     scheduler = BlockingScheduler(timezone=SEOUL)
 
     # 뉴스 API 배치 스케쥴: 시간 변경 가능
     scheduler.add_job(job_news, CronTrigger(hour=21, minute=41, timezone=SEOUL), id="news_batch")
+
+    # Top 10 기업 추출 연산 배치 스케쥴: 시간 변경 가능
+    scheduler.add_job(job_top10, CronTrigger(hour=0, minute=36, timezone=SEOUL), id="top10_batch")
 
     logger.info("스케쥴러가 시작되었습니다.") 
 
