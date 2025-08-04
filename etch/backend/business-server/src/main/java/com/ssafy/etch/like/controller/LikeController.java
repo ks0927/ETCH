9a71224806923e545b45671495745719fa6cbabd
이재빈 <woqls100@daum.net>
2025-08-04 17:40:23 +0900

    feat: feat: Add like feature for company content
    기업 컨텐츠에 대한 좋아요 기능 추가
    
    S13P11A402-204-api

--- etch/backend/business-server/src/main/java/com/ssafy/etch/like/controller/LikeController.java
+++ etch/backend/business-server/src/main/java/com/ssafy/etch/like/controller/LikeController.java
@@ -33,4 +33,16 @@ public class LikeController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.success(null, "뉴스 좋아요가 등록되었습니다."));
     }
+    @PostMapping("/companies")
+    public ResponseEntity<ApiResponse<Object>> createLikeCompany(@AuthenticationPrincipal CustomOAuth2User oAuth2User,
+                                                              @RequestBody LikeRequestDTO likeRequestDTO) {
+        if (oAuth2User == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(ApiResponse.error("인증되지 않은 사용자입니다."));
+        }
+
+        likeService.saveLike(oAuth2User.getId(), likeRequestDTO, LikeType.COMPANY);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success(null, "뉴스 좋아요가 등록되었습니다."));
+    }
 }
