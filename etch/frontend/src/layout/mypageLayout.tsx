fb8f1983f6fa125130591cc3aa32be14d344cbc8
ks0927 <brian_als@naver.com>
2025-08-06 08:33:04 +0900

    feat: 마이페이지 레이아웃 추가

---
+++ etch/frontend/src/layout/mypageLayout.tsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router";
+import NavMenu from "../components/organisms/mypage/navMenu";
+import ProfileCard from "../components/organisms/mypage/profileCard";
+
+const MyPageLayout = () => {
+  return (
+    // Layout의 main 안에 들어갈 내용
+    <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+      {/* 왼쪽 사이드바 */}
+      <div className="lg:col-span-1 space-y-4">
+        <ProfileCard />
+        <NavMenu />
+      </div>
+
+      {/* 오른쪽 메인 콘텐츠 영역 */}
+      <div className="lg:col-span-3">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default MyPageLayout;
