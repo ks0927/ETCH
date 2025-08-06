2641a4542f5de7472f45ac79791a23393d4e8902
ks0927 <brian_als@naver.com>
2025-08-06 10:43:58 +0900

    refactor: svg 분리

--- etch/frontend/src/components/molecules/chat/chatButton.tsx
+++ etch/frontend/src/components/molecules/chat/chatButton.tsx
@@ -1,10 +1,10 @@
 import type { ButtonProps } from "../../atoms/button";
-import chatIcon from "../../../assets/chat.svg";
+import chatIcon from "../../svg/chat.svg";
 
 export default function ChatButton({ onClick }: ButtonProps) {
   return (
     <button
-      className="fixed bottom-6 right-6 z-50 cursor-pointer hover:scale-110 transition-transform duration-200"
+      className="fixed z-50 transition-transform duration-200 cursor-pointer bottom-6 right-6 hover:scale-110"
       onClick={onClick}
     >
       <img src={chatIcon} alt="Chat" className="w-20 h-20" />
