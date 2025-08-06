99ad58597fdc377d15f9cd9157ae364871efa497
hyunji1132 <hyunji1132@gmail.com>
2025-08-06 16:37:38 +0900

    fix: fix API to connect Redis

--- etch/backend/business-server/src/main/java/com/ssafy/etch/company/service/CompanyServiceImpl.java
+++ etch/backend/business-server/src/main/java/com/ssafy/etch/company/service/CompanyServiceImpl.java
@@ -1,17 +1,9 @@
 package com.ssafy.etch.company.service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.etch.company.dto.CompanyInfoDTO;
-import com.ssafy.etch.company.dto.CompanyRankingDTO;
 import com.ssafy.etch.company.entity.CompanyEntity;
 import com.ssafy.etch.company.repository.CompanyRepository;
 
@@ -24,36 +16,6 @@ public class CompanyServiceImpl implements CompanyService {
 	public CompanyServiceImpl(RedisTemplate<String, String> redisTemplate,  CompanyRepository companyRepository) {
 		this.redisTemplate = redisTemplate;
 		this.companyRepository = companyRepository;
-	}
-
-	// 좋아요가 많은 순으로 정렬한 10개의 기업 - 수정 필요
-	@Override
-	public List<CompanyRankingDTO> getTop10() {
-		ListOperations<String, String> listOps = redisTemplate.opsForList();
-		HashOperations<String, String, String> hashOps = redisTemplate.opsForHash();
-
-		List<String> keys = listOps.range("top10_companies", 0, -1);
-		if (keys == null || keys.isEmpty()) return List.of();
-
-		List<CompanyRankingDTO> result = new ArrayList<>();
-		for (String key : keys) {
-			Map<String, String> info = hashOps.entries(key);
-
-			if (info.isEmpty()) continue;
-
-			String name = info.getOrDefault("company_name", "");
-			long likes = 0L;
-
-			try {
-				likes = Long.parseLong(info.getOrDefault("likes", "0"));
-			} catch (NumberFormatException ignored) {
-			}
-
-			result.add(new CompanyRankingDTO(name, likes));
-		}
-
-			result.sort(Comparator.comparingLong(CompanyRankingDTO::getLikes).reversed());
-			return result.size() > 10 ? result.subList(0, 10) : result;
 	}
 
 	// 기업정보
--- etch/backend/business-server/src/main/java/com/ssafy/etch/news/service/NewsService.java
+++ etch/backend/business-server/src/main/java/com/ssafy/etch/news/service/NewsService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import com.ssafy.etch.news.dto.CompanyNewsDTO;
 import com.ssafy.etch.news.dto.LatestNewsDTO;
+import com.ssafy.etch.news.dto.TopCompanyDTO;
 
 public interface NewsService {
 	List<LatestNewsDTO> getLatestNews();
 	List<CompanyNewsDTO> getNewsByCompanyId(Long companyId);
+	List<TopCompanyDTO> getTopCompaniesFromRedis();
 }
