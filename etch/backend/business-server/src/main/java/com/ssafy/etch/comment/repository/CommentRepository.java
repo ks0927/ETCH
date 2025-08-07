d8d5ee7cc5bd320178e2ac09eeaa595774f76f50
한승수 <h2sorginal@gmail.com>
2025-08-07 11:57:01 +0000

    fix: remove @repository

--- etch/backend/business-server/src/main/java/com/ssafy/etch/comment/repository/CommentRepository.java
+++ etch/backend/business-server/src/main/java/com/ssafy/etch/comment/repository/CommentRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import com.ssafy.etch.comment.entity.CommentEntity;
 
-@Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
 	List<CommentEntity> findAllByProjectIdOrderByCreatedAtDesc(Long projectId);
 }
