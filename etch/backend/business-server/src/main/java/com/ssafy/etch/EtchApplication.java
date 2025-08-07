83ae1962cb2d274f2bd7131d6ee7c0e9837f4175
한승수 <h2sorginal@gmail.com>
2025-08-07 11:40:52 +0000

    fix: for ec2 build

--- etch/backend/business-server/src/main/java/com/ssafy/etch/EtchApplication.java
+++ etch/backend/business-server/src/main/java/com/ssafy/etch/EtchApplication.java
@@ -3,7 +3,8 @@ package com.ssafy.etch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ssafy.etch")
 public class EtchApplication {
 
     public static void main(String[] args) {
