c650bb658b294b62a5bcb5e037f8c61fb59137b2
이재빈 <woqls1009@daum.net>
2025-08-08 01:21:23 +0900

    feat: implement user cover-letter registration
    사용자 자기소개서 등록 기능 구현
    
    S13P11A402-68-api

--- etch/backend/business-server/src/main/java/com/ssafy/etch/coverLetter/service/CoverLetterService.java
+++ etch/backend/business-server/src/main/java/com/ssafy/etch/coverLetter/service/CoverLetterService.java
@@ -1,9 +1,11 @@
 package com.ssafy.etch.coverLetter.service;
 
 import com.ssafy.etch.coverLetter.dto.CoverLetterListResponseDTO;
+import com.ssafy.etch.coverLetter.dto.CoverLetterRequestDTO;
 
 import java.util.List;
 
 public interface CoverLetterService {
     List<CoverLetterListResponseDTO> getCoverLetterlist(Long memberId);
+    void saveCoverLetter(Long memberId, CoverLetterRequestDTO coverLetterRequestDTO);
 }
--- etch/backend/business-server/src/main/java/com/ssafy/etch/coverLetter/service/CoverLetterServiceImpl.java
+++ etch/backend/business-server/src/main/java/com/ssafy/etch/coverLetter/service/CoverLetterServiceImpl.java
@@ -1,8 +1,16 @@
 package com.ssafy.etch.coverLetter.service;
 
+import com.ssafy.etch.coverLetter.dto.CoverLetterDTO;
 import com.ssafy.etch.coverLetter.dto.CoverLetterListResponseDTO;
+import com.ssafy.etch.coverLetter.dto.CoverLetterRequestDTO;
 import com.ssafy.etch.coverLetter.entity.CoverLetterEntity;
 import com.ssafy.etch.coverLetter.repository.CoverLetterRepository;
+import com.ssafy.etch.global.exception.CustomException;
+import com.ssafy.etch.global.exception.ErrorCode;
+import com.ssafy.etch.member.entity.MemberEntity;
+import com.ssafy.etch.member.repository.MemberRepository;
+import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +21,7 @@ import java.util.List;
 public class CoverLetterServiceImpl implements CoverLetterService {
 
     private final CoverLetterRepository coverLetterRepository;
+    private final MemberRepository memberRepository;
 
     @Override
     public List<CoverLetterListResponseDTO> getCoverLetterlist(Long memberId) {
@@ -21,5 +30,20 @@ public class CoverLetterServiceImpl implements CoverLetterService {
                 .map(CoverLetterEntity::toCoverLetterDTO)
                 .map(CoverLetterListResponseDTO::from)
                 .toList();
+    }
+
+    @Override
+    @Transactional
+    public void saveCoverLetter(Long memberId, CoverLetterRequestDTO coverLetterRequestDTO) {
+        MemberEntity memberEntity = memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        CoverLetterDTO coverLetterDTO = coverLetterRequestDTO.toCoverLetterDTO()
+                .toBuilder()
+                .member(memberEntity)
+                .build();
+        CoverLetterEntity entity = CoverLetterEntity.from(coverLetterDTO);
+
+        coverLetterRepository.save(entity);
     }
 }
