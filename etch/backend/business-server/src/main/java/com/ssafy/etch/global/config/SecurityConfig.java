896e7232ac2f9724a053c94d2882ab9969c58c65
DESKTOP-HA97GMI\SSAFY <h2sorginal@gmail.com>
2025-08-08 13:18:43 +0900

    add: /members permition test

--- etch/backend/business-server/src/main/java/com/ssafy/etch/global/config/SecurityConfig.java
+++ etch/backend/business-server/src/main/java/com/ssafy/etch/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
@@ -83,6 +84,7 @@ public class SecurityConfig {
 		http.authorizeHttpRequests(
 			(auth) -> auth.requestMatchers("/**", "/signup", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
 				.permitAll()
+				.requestMatchers(HttpMethod.POST, "/members/").hasAnyRole("GUEST")
 				.requestMatchers("/user/**", "/auth/me")
 				.hasAnyRole("GUEST", "USER")
 				.anyRequest()
