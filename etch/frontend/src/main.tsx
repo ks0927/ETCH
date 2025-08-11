0e91f0fee5d13ef6348547d30585abafe468aefe
한승수 <h2sorginal@gmail.com>
2025-08-11 00:14:38 +0000

    fix: fix for blue/green deploy

--- etch/frontend/src/main.tsx
+++ etch/frontend/src/main.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./router/router.tsx";
 
-//main.tsx 
+//main.ts!!x 
 createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router}></RouterProvider>
 );
