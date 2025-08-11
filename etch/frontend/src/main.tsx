9fe2eaf4ae7b2ab6eacb0b45ec0c28b39b4c9674
한승수 <h2sorginal@gmail.com>
2025-08-11 03:38:32 +0000

    fix: fix for deploy test

--- etch/frontend/src/main.tsx
+++ etch/frontend/src/main.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./router/router.tsx";
 
-//main.ts!!x 
+//main.ts!!!!x 
 createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router}></RouterProvider>
 );
