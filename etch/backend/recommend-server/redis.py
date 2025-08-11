ad99e24d2ae812698c3c27cf85d8563fc2edf097
이재빈 <woqls100@daum.net>
2025-08-11 18:53:45 +0900

    refactor: Extract and inject configuration values via environment variables
    하드코딩된 설정 값을 환경변수로 추출하고, 애플리케이션 실행 시 주입받아 사용하도록 수정
    
    S13P11A402-284-fastapi-api

--- etch/backend/recommend-server/redis.py
+++ etch/backend/recommend-server/redis.py
@@ -1,11 +1,16 @@
 import json
 # redis_connection.py
 import redis.asyncio as redis
+import os
+from dotenv import load_dotenv
+
+# .env 파일에서 환경 변수 로드
+load_dotenv()
 
 redis_client = redis.Redis(
-    host="localhost",
-    port=6379,
-    db=0,
+    host=os.getenv("REDIS_HOST"),
+    port=int(os.getenv("REDIS_PORT")),
+    db=int(os.getenv("REDIS_DB")),
     decode_responses=True  # 문자열 변환
 )
 async def store_recommendations(user_id, news_recommendations, job_recommendations):
