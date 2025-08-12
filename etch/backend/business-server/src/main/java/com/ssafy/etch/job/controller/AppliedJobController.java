5ac9d0a0ba4706fc1942e822d1d3bbb785e3980e
이재빈 <woqls100@daum.net>
2025-08-12 19:49:55 +0900

    docs: Add tag annotation to controller for API documentation
    컨트롤러에 태그 추가
    
    dev

--- etch/backend/business-server/src/main/java/com/ssafy/etch/job/controller/AppliedJobController.java
+++ etch/backend/business-server/src/main/java/com/ssafy/etch/job/controller/AppliedJobController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @Tag(
         name = "AppliedJobs",
-        description = "AppliedJobs Controller 입니다."
+        description = "AppliedJobs Controller 입니다. "
 )
 @RestController
 @RequiredArgsConstructor
