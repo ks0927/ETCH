b0029378ad9f797b2a0220a33c72e4903f67a6dc
DESKTOP-HA97GMI\SSAFY <h2sorginal@gmail.com>
2025-08-14 11:10:13 +0900

    fix: allow origin config open etch.it.kr

--- etch/backend/chat-server/src/main/java/com/ssafy/chat/config/SecurityConfig.java
+++ etch/backend/chat-server/src/main/java/com/ssafy/chat/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5175"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:5175", "https://etch.it.kr"));
         configuration.setAllowedMethods(Collections.singletonList("*"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowCredentials(true);
