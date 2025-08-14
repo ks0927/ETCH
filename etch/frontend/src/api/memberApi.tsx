92669f0cc09ef8449ac506c6b1edec9b59960be1
ks0927 <brian_als@naver.com>
2025-08-14 14:26:03 +0900

    feat: 유저 사진 api 연동

--- etch/frontend/src/api/memberApi.tsx
+++ etch/frontend/src/api/memberApi.tsx
@@ -1,5 +1,6 @@
 import { authInstance } from "./instances"; // authInstance를 import
 import type { UserProfile } from "../types/userProfile";
+import type { ProfileImageUpdateResponse } from "../types/member";
 
 interface ApiResponse<T> {
   success: boolean;
@@ -11,4 +12,16 @@ export const getMemberById = async (memberId: number): Promise<UserProfile> => {
   // authInstance를 사용하여 API 호출
   const response = await authInstance.get<ApiResponse<UserProfile>>(`/members/${memberId}`);
   return response.data.data;
+};
+
+export const updateProfileImage = async (file: File): Promise<string> => {
+  const formData = new FormData();
+  formData.append('profile', file);
+  
+  const response = await authInstance.patch<ApiResponse<ProfileImageUpdateResponse>>(
+    '/members/profile-update',
+    formData
+  );
+  
+  return response.data.data.profileImageUrl;
 };
